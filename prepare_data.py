3368c198e2559b9ddbd94c121423f701243b8c82
Thomas Wood <odell.wood@gmail.com>
2022-04-09 23:02:27 -0700

    Added the filenames of the input and output files to prepare_data.py into config.yaml

--- prepare_data.py
+++ prepare_data.py
@@ -1,11 +1,15 @@
 import re
 import json
 
-def load_book():
-    with open('tale_of_two_cities.txt', 'r') as f:
+from util import get_cfg
+from typing import List
+
+def load_book(cfg: dict) -> str:
+    book_text_filename = cfg['book_text_filename']
+    with open(book_text_filename, 'r') as f:
         return f.read()
 
-def chapterize(book):
+def chapterize(book: str) -> List(dict):
     pattern = r'CHAPTER'
     chapters = re.split(pattern, book, flags=re.DOTALL|re.MULTILINE)
     chapter_start = 46 # Determined by manual investigation
@@ -13,14 +17,16 @@ def chapterize(book):
     chapters = [chapters[k].strip() for k in range(chapter_start, len(chapters))]
     return [{'text': x} for x in chapters]
 
-def save_book(book):
-    with open('tale2cities.json','w') as f:
+def save_book(book: List(dict), cfg: dict) -> None:
+    book_json_filename = cfg['book_json_filename']
+    with open(book_json_filename,'w') as f:
         json.dump(book, f)
 
-def prepare_data():
-    book = load_book()
+def prepare_data(cfg: dict) -> None:
+    book = load_book(cfg)
     chapters = chapterize(book)
-    save_book(chapters)
+    save_book(chapters, cfg)
 
 if __name__ == '__main__':
+    cfg = get_cfg()
     prepare_data()
